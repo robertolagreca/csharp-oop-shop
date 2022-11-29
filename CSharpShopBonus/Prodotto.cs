ab617a20931362cf7582808ed5db3523cdcedba2
robertolagreca <robertolagreca92@gmail.com>
2022-11-29 16:39:09 +0100

    Bonus, aggiunta zeri al codice.

---
+++ CSharpShopBonus/Prodotto.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop
+{
+    public class Prodotto
+    {
+        //dichiariazione variabili private
+        private int code;
+        private string name;
+        private string description;
+        private float price;
+        private int iva;
+
+        
+        
+        //COSTRUTTORE
+        public Prodotto(int code, string name, string description, float price, int iva)
+        {
+            this.code = code;
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.iva = iva;
+        }
+
+        
+        
+        //SETTERS
+
+        public void SetName (string name)
+        {
+            this.name = name;
+        }
+
+        public void SetDescription (string description)
+        {
+            this.description = description;
+        }
+
+        public void SetPrice (float price)
+        {
+            this.price = price;
+        }
+
+        public void SetIva(int iva)
+        {
+            this.iva = iva;
+        }
+
+        /*public void SetCode (int code)
+       {
+           this.code = code;
+       }*/
+
+
+
+
+        //GETTERS
+
+        public int GetCode() { return code; }
+        public string GetName() { return name; }
+        public string GetDescription() { return description; }
+        public float GetPrice() { return price; }
+        public int GetIva() { return iva; }
+
+        
+        
+        //METODI
+
+        //Generazione numero casuale
+        public void CreateCode()
+        {   
+            Random rnd = new Random();
+            int randCode;
+            randCode = rnd.Next(1, 99999999);
+            code = randCode;
+            
+        }
+
+        //Stampa prezzo senza IVA
+        public void BasicPrice(float price)
+        {
+            Console.WriteLine("Il prezzo base è " + price + " euro");
+        }
+
+        //Stampa prezzo con IVA
+        public void FullPrice(float price, int iva)
+        {
+            float fullPrice;
+
+            fullPrice = (float)Math.Round(price + (price * (iva / 100f)), 2);
+
+            Console.WriteLine("Il prezzo compreso di IVA è  " + fullPrice.ToString() + " euro");
+        }
+
+        //Stampa nome+codice
+        public void FullName(string name, int code)
+        {
+            string fullName;
+
+            fullName = code.ToString() + name; 
+            Console.WriteLine("Il nome completo è " + fullName);
+        }
+
+        public void PadLeftCode(int code)
+        {
+            string codeStr;
+            int maxChars = 8;
+            int missedChars = 0;
+            codeStr = code.ToString();
+
+            missedChars = maxChars - codeStr.Length;
+
+            for (int i = 0; i < missedChars; i++)
+                {
+                    codeStr = "0" + codeStr;
+             
+            }
+            
+           Console.WriteLine("Codice con pad left: " +codeStr);
+         
+        }
+
+    }
+}
---
+++ CSharpShopBonus/Program.cs
@@ -0,0 +1,104 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Nel progetto CSharpShop , creare la classe Prodotto che gestisce i prodotti del vostro shop.
+//Un prodotto è caratterizzato da:
+//codice(numero intero)
+//nome
+//descrizione
+//prezzo
+//iva
+
+//Usate opportunamente i livelli di accesso (public, private), i costruttori,
+//i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+//alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random.
+//Il codice prodotto sia accessibile solo in lettura
+//Gli altri attributi siano accessibili sia in lettura che in scrittura
+//Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva.
+//Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+//Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interagirci
+//con loro per testare tutti i metodi che avete previsto e implementato.
+
+//BONUS:
+//create un metodo che restituisca il codice con un pad left di 0 per arrivare
+//a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+//Usando una List, dichiarate una lista dei prodotti di un negozio e inseriteci
+//al suo interno qualche prodotto che vi aspettate di trovare nel negozio.
+//Stampate poi l’elenco dei vostri prodotti che avete previsto nel negozio.
+
+using CSharpShop;
+using System.Runtime.CompilerServices;
+
+//Dichiariazione viariabili
+int code;
+int iva;
+float price;
+string name;
+string description;
+
+
+
+//Assegnazione valori esempio
+code = 0;
+iva = 22;
+price = 12.99f;
+name = "I sentieri dei nidi di ragno";
+description = "Autore Italo Calvino";
+
+
+
+//creazione oggetti.
+Prodotto prodObject = new Prodotto(code, name, description, price, iva);
+
+
+//Creazione codice random
+prodObject.CreateCode();
+
+
+//Input utente variabili
+Console.WriteLine("Inserisci nome, descrizione, prezzo e iva");
+
+name = Console.ReadLine();
+description =Console.ReadLine();
+price = float.Parse(Console.ReadLine());
+iva = int.Parse(Console.ReadLine());
+
+
+
+
+//Set delle variabili nell'oggetto
+prodObject.SetName(name);
+prodObject.SetDescription(description);
+prodObject.SetPrice(price);
+prodObject.SetIva(iva);
+
+
+
+
+//Richiedo variabili a classe Prodotto e Stampa
+code = prodObject.GetCode();
+Console.WriteLine("Il codice creato per il tuo libro è " + code);
+
+Console.WriteLine("Hai inserito i seguenti dati:");
+
+Console.WriteLine(prodObject.GetName());
+Console.WriteLine(prodObject.GetDescription());
+
+
+
+//Metodo che restituisce codice + nome
+prodObject.FullName(name, code);
+
+
+
+//Metodo che restituisce il prezzo base
+prodObject.BasicPrice(price);
+//Console.WriteLine(prodObject.GetPrice());
+
+
+
+//Metodo che restituisce il prezzo compreso di IVA.
+prodObject.FullPrice(price, iva);
+//Console.WriteLine(prodObject.GetIva());
+
+//Metodo che aggiunge tanti zeri fino a 8 caratteri al codice
+prodObject.PadLeftCode(code);
